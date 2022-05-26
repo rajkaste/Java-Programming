c47fbdd5ffcfbb95f1d58ff5db3235c41ab2804c
Raj Kaste <rajkaste@gmail.com>
2022-05-26 17:31:14 +0530

    Create Employee.java

---
+++ Employee/Employee.java
@@ -0,0 +1,18 @@
+public class Employee {
+
+    String name;
+    float basicsalary, grosssalary;
+
+    Employee(String name, float basicsalary){
+        this.name = name;
+        this.basicsalary = basicsalary;
+    }
+
+    public void getGrosssalary() {
+        float DA,HRA;
+        DA = 40 * basicsalary /100;
+        HRA = 20 * basicsalary /100;
+        grosssalary = basicsalary + DA + HRA;
+    }
+
+}
